4cabcc496cab2247983c69800bf000803eebb279
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-07-25 20:49:29 -0400

    Implemented a second path in the Router for future use and proof-of-concept

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Navigation from '../components/molecules/Navigation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigation/>
         {this.props.children}
       </div>
     );
--- app/src/routes.js
+++ app/src/routes.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { IndexRoute, Router, Route, hashHistory } from "react-router";
-import Home from './containers/Home';
 import App from './containers/App';
+import Home from './components/pages/Home';
+import Test from './components/pages/Test';
+
 
 export default (
     <Router history={hashHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Home}/>
+        </Route>
+        <Route path="/test" component={App}>
+            <IndexRoute component={Test}/>
         </Route>
     </Router>
 );
