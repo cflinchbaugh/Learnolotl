6619c5f06330b7be1f69db503df11d89a6f27f78
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-08-04 17:42:35 -0400

    Created a fade animation im preparation for react-transition-groupo

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //Global Styles pulled in here
 import Fonts from '../styles/fonts.css';
 import Util from '../styles/util.css';
+import Animations from '../styles/animations.css';
 import Router from './Router';
 
 export default(
