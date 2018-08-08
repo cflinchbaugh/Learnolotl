72410c01570fbf2d12be52ebb216ee18e0dd039d
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-08-08 18:34:32 -0400

    Implemented ThemeProvider and created a colors file!

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -4,9 +4,13 @@ import Fonts from '../styles/fonts.css';
 import Util from '../styles/util.css';
 import Animations from '../styles/animations.css';
 import Router from './Router';
+import {styled, ThemeProvider} from 'styled-components';
+import theme from '../styles/colors';
 
 export default(
-  <div className="App">
-    <Router/>
-  </div>
+  <ThemeProvider theme={theme}>
+    <div className="App">
+      <Router/>
+    </div>
+  </ThemeProvider>
 );
---
+++ app/src/styles/colors.js
@@ -0,0 +1,25 @@
+let dark = '#000000',
+    light = '#ffffff';
+
+
+export default {
+    // Primary
+        primary: '#724dff',
+        primaryColor: light,
+
+        primaryLight: '#b47cff',
+        primaryLightColor: dark,
+
+        primaryDark: '#3f1dcb',
+        primaryDarkColor: light,
+
+    //Secondary
+        secondary: '#1de9b6',
+        secondaryColor: dark,
+        
+        secondaryLight: '#6effe8',
+        secondaryLightColor: dark,
+        
+        secondaryDark: '#00b686',
+        secondaryDarkColor: dark
+};
