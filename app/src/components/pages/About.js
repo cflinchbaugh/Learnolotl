5c6c973c6bf1d0dbba10da5dd7dd4d9592ffd541
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-08-08 20:40:00 -0400

    Added some text to the About page

--- app/src/components/pages/About.js
+++ app/src/components/pages/About.js
@@ -7,8 +7,17 @@ class About extends Component {
             <div>
                 <h1>About</h1>
                 <div>
-                    Version: 0.0.0
-                    <a target="_blank" rel="noopener noreferrer" href="https://github.com/cflinchbaugh/level-up">GitHub Repo</a>
+                    <p>Version: 0.0.0</p>
+                    <p>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/cflinchbaugh/level-up">GitHub Repo</a>
+                    </p>
+                    <p>
+                        A passion project built primarily to assist with my own study of the Japanese language, but designed to be flexible and practical for several learning use-cases.<br/><br/>
+
+                        GitHub description: "A flexible, JSON-eating study-aid application, great for language learning and other memorization-based studying."<br/><br/>
+
+                        Built with ReactJS. Conceived, designed, architected, and implemented by <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/christopher-flinchbaugh/">me</a>.<br/><br/>
+                    </p>
                 </div>
             </div>
         );
