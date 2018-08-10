d58c332cf16be89d30e893eedf16b9583fedfdf0
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-08-10 18:53:20 -0400

    Created theme color 'dark' and implemented, isolating hexcodes to the colors file just in case...

--- app/src/components/atoms/ActiveItem.js
+++ app/src/components/atoms/ActiveItem.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyleWrapper = styled.div`
-    color: ${props => props.theme.primaryColor};
+    color: ${props => props.theme.dark};
     font-size: 3em;
-    text-shadow: 0px 6px 3px rgba(0,0,0,0.4),
+    text-shadow: 0px 6px 3px rgba(0,0,0,0.3),
          0px 8px 13px rgba(0,0,0,0.1),
          0px 18px 23px rgba(0,0,0,0.1);
 `;
@@ -13,9 +13,7 @@ const StyleWrapper = styled.div`
 function ActiveItem (props) {
     return (
         <StyleWrapper>
-            <div className="active-item">
-                { props.value }
-            </div>
+            { props.value }
         </StyleWrapper>
     )
 }
