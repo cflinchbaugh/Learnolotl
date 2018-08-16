7191c74fdce253774fa371045b45774a2714d2e8
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-08-15 22:13:02 -0400

    Renamed the actions/types file from actions/cards

--- app/src/containers/store.js
+++ app/src/containers/store.js
@@ -1,6 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from '../reducers/index';
+import rootReducer from '../reducers';
 
 const initialState = {};
 
@@ -9,7 +9,10 @@ const middleWare = [thunk];
 const store = createStore(
         rootReducer,
         initialState,
-        applyMiddleware(...middleWare)
+        compose(
+            applyMiddleware(...middleWare),
+            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
     );
 
 export default store;
