0962b315e0920e4d9618069ebd73750ea76b7ec0
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-08-24 17:34:52 -0400

    Resolved Redux Devtools Extension bug browsers causing app to fail if a browser did not have it installed

--- app/src/containers/store.js
+++ app/src/containers/store.js
@@ -1,17 +1,18 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 
 const middleWare = [thunk];
 
+
 const store = createStore(
-        rootReducer,
+    rootReducer,
         initialState,
-        compose(
-            applyMiddleware(...middleWare),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        composeWithDevTools (
+            applyMiddleware(...middleWare)
         )
     );
 
