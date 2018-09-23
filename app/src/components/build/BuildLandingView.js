bad0fa7806821c91bf0cb028243311fa52f6a30c
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-09-23 00:40:01 -0400

    Build text cleanup, rewording for enhanced conciseness

--- app/src/components/build/BuildLandingView.js
+++ app/src/components/build/BuildLandingView.js
@@ -26,9 +26,6 @@ function BuildLandingView (props) {
                 You want to learn more than just the few sample cards that came with the app?  No problem!
                 It's easy to make your own Memorinity cards and share them with others.
             </p>
-            <p>
-                First, what do you want this set of cards to be called?  We'll use it as a filename, so please, just letters and underscores.
-            </p>
             <NavigationArrow {...navigationArrowData} />
         </StyleWrapper>
     )
