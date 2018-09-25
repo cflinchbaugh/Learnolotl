b4ca5828776643aff444b2779823e625c1b41550
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-09-25 19:39:50 -0400

    Implemented a disabled state on the navigation-button and applied it on the FileId Build screen

--- app/src/components/buttons/NavigationArrow.js
+++ app/src/components/buttons/NavigationArrow.js
@@ -1,34 +1,48 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyleWrapper = styled.button`
-    color: orange;
-    background: transparent;
-    border: solid 1px orange;
-    transition: all 0.35s; 
-    display: inline-block;
-    border-radius: 15px;
-    padding: 8px 20px;
-    box-shadow: 1px 8px 19px 3px rgba(61,55,61,0.25);
-    user-select: none;
-
-    &:hover {
-        cursor: pointer;
-        background-color: #566184;
-        box-shadow: 1px 8px 19px 3px rgba(61,55,61,0.48);
+const StyleWrapper = styled.div`
+    .navigation-button {
+        color: orange;
+        background: transparent;
+        border: solid 1px orange;
+        transition: all 0.35s; 
+        display: inline-block;
+        border-radius: 15px;
+        padding: 8px 20px;
+        box-shadow: 1px 8px 19px 3px rgba(61,55,61,0.25);
+        user-select: none;
+        
+        &:hover {
+            cursor: pointer;
+            background-color: #566184;
+            box-shadow: 1px 8px 19px 3px rgba(61,55,61,0.48);
+        }
+    }
+    
+    .disabled {
+        border: solid 1px darkgray;
+        color: darkgray;
+        
+        &:hover {
+            cursor: not-allowed;
+            background-color: transparent;
+        }
     }
 `
 
 function NavigationArrow (props) {
     let arrow = props.type === 'next' ? '=>' : '<=',
         navigationArrowData ={
-            className: 'navigation-button',
+            className: props.disabled ? 'navigation-button disabled' : 'navigation-button',
             onClick: props.onClickFunction
         }
 
     return (
-        <StyleWrapper {...navigationArrowData} >
-            {arrow}
+        <StyleWrapper className='navigation-button-wrapper'>
+            <button {...navigationArrowData} >
+                {arrow}
+            </button>
         </StyleWrapper>
     )
 }
