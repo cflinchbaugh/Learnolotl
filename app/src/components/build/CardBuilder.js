af9461adab27b7e7bd03e6a55b435f2183c02c09
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-09-27 20:09:52 -0400

    Organizing CardBuilder content

--- app/src/components/build/CardBuilder.js
+++ app/src/components/build/CardBuilder.js
@@ -14,11 +14,15 @@ const StyleWrapper = styled.div`
 function CardBuilder (props) {
     return (
         <StyleWrapper className='card-builder-wrapper clearfix'>
-            <p>Now, fill in as many cards as you like!  When you're all finished, click Download and you'll have a brand new set of Memorinity cards to use in the Learn section!</p>
+            <p>Now, fill in as many cards as you like!</p>
             <form onSubmit={props.handleSubmitForm}>
                 <FormElementFactory {...props.formElementFactoryData}/>
                 <Button {...props.nextCardButtonData}/>
 
+                <p>
+                    When you're all finished, click Download and you'll have a brand new set of Memorinity cards to use in the Learn section!
+                </p>
+                
                 <div className="export-button-wrapper">
                     <Button {...props.exportButtonData}/>
                 </div>
