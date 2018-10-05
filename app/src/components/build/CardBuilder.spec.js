e0a660b9d1babeb3d501b7929907e8a8a1c6ecd9
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-10-04 23:43:57 -0400

    Moved unit test to be a peer of actual component, still not passing but it is running

--- app/src/components/build/CardBuilder.spec.js
+++ app/src/components/build/CardBuilder.spec.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import CardBuilder from '../CardBuilder';
+import CardBuilder from './CardBuilder';
 
 describe('CardBuilder', () => {
     it('should call handleSubmitForm when the form is submitted', () => {
-    const handleSubmitForm = jest.fn();
+        const handleSubmitForm = jest.fn();
     
-    const wrapper = shallow(<CardBuilder handleSubmitForm={handleSubmitForm}/>);
+        const wrapper = shallow(<CardBuilder handleSubmitForm={handleSubmitForm}/>);
 
         wrapper.find('form').simulate('submit');
 
