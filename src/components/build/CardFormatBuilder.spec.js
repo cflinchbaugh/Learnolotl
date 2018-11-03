fc67736e18eed6bcf6b66ce5b2503dd118e6c226
cflinchbaugh <christopher.e.flinchbaugh@gmail.com>
2018-11-03 16:13:43 -0400

    Cleaned up snapshot tests

--- src/components/build/CardFormatBuilder.spec.js
+++ src/components/build/CardFormatBuilder.spec.js
@@ -6,9 +6,15 @@ import ShallowRenderer from 'react-test-renderer/shallow';
 describe('CardFormatBuilder', () => {
     it('renders and matches snapshot', () => {
         const renderer = new ShallowRenderer();
+        let cardFormatBuildData = {
+            saveFormatButtonData: {
+                type: 'next',
+                onClickFunction: function(){}
+            }
+        }
 
         const tree = renderer.render(
-            <CardFormatBuilder />
+            <CardFormatBuilder {...cardFormatBuildData}/>
         );
 
         expect(tree).toMatchSnapshot();
